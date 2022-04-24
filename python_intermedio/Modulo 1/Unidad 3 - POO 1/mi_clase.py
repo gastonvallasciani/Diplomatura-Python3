ef35291ea68dc4f3229c57201ed6629702c86247
gaston <gaston.vallasciani@pump-control.com>
2022-04-24 16:23:11 -0300

    Update unit 3 POO class exercises.

---
+++ python_intermedio/Modulo 1/Unidad 3 - POO 1/mi_clase.py
@@ -0,0 +1,14 @@
+class OperacionesM():
+
+    def sumar(self, a ,b):
+        print(id(self))
+        c = a + b
+        return c
+
+# Instancio el objeto de la clase OperacionesM()
+obj = OperacionesM()
+# Ejecuto el metodo "sumar" de la clase OperacionesM()
+print(obj.sumar(2, 3))
+# verifico la posicion en memoria del objeto creado es la misma que la de "self" dentro de la clase
+# El objeto es referenciado dentro de la clase por medio de "self"
+print(id(obj))
